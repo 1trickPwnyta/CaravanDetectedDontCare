21b02497218ad1342cefe4e0fb5d22d1d6687d78
1trickPwnyta <sarahpace37@gmail.com>
2025-06-12 15:26:15 -0700

    Update to 1.6

---
+++ 1.6/Source/Patch_TimedDetectionRaids.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RimWorld.Planet;
+
+namespace CaravanDetectedDontCare
+{
+    [HarmonyPatch(typeof(TimedDetectionRaids))]
+    [HarmonyPatch("NotifyPlayer")]
+    public static class Patch_TimedDetectionRaids
+    {
+        public static bool Prefix(TimedDetectionRaids __instance)
+        {
+            __instance.alertRaidsArrivingIn = true;
+            return false;
+        }
+    }
+}
